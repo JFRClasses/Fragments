6f873beb64bf3ef159a168b41056ea2dbb5bf8f2
juanfrausto <juanfr97@hotmail.com>
2024-03-12 17:27:23 -0600

    Fragments implemented

--- app/src/main/java/com/example/fragmentsapp/MainActivity.kt
+++ app/src/main/java/com/example/fragmentsapp/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.fragmentsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fragmentsapp.fragments.CategoryFragment
+import com.example.fragmentsapp.fragments.HomeFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavbar: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavbar = findViewById(R.id.bottomNavigationView)
+        val homeFragment = HomeFragment()
+        replaceFragment(homeFragment)
+        bottomNavbar.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.home -> {
+                    val homeFragment = HomeFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, homeFragment).commit()
+                }
+                R.id.category -> {
+                    val categoryFragment = CategoryFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, categoryFragment).commit()
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: HomeFragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
     }
 }
